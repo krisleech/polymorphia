50c03312e3e9515e26ee5fae5c7d6c3b2851c1be
Kris Leech <kris.leech@interkonect.com>
2012-01-24 21:39:49 +0000

    Working out some specs

--- spec/lib/associate_spec.rb
+++ spec/lib/associate_spec.rb
@@ -1,15 +1,20 @@
 require 'spec_helper'
 
-
 describe Associate do
   before(:each) do
     Associate.destroy_all
-  end
-
-  it 'joins any two models' do
     person = mock('person')
     university = mock('university')
+  end
 
+  it '#destroy_all deletes all associations'
+
+  it '#this(object) sets the source object'
+  
+  it '#to(object) sets the target object'
+
+  it '#commit! creates the association' do
     Associate.this(person).to(university).commit!.should be_true
   end
+
 end
